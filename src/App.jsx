39d3c9582884ad592b36e6aeeca95f50e6d2ec83
guanzhen <406873093@qq.com>
2019-09-09 20:51:59 +0800

    updata,路由 实现

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
+//路由库
+import {BrowserRouter as Router, Switch,Route} from 'react-router-dom';
+import Home from "./components/home";
+import Login from "./components/login";
 
-export default class App extends React.Component{
-    constructor(props){
+export default class App extends React.Component {
+    constructor(props) {
         super(props)
     };
 
     render() {
         return (
-            <div>
-                app
-            </div>
+            <Router>
+                <p>app</p>
+                <Switch>
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/login" exact component={Login}/>
+                </Switch>
+            </Router>
         );
     };
 
